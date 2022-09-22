9e68b94762c4ab7e31c20c6502ed87a5de764d9f
Brandon Moye <brandon.m.moye@gmail.com>
2022-09-21 21:24:28 -0500

    commint to maste

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "./headerStyle.css";
+
+export default function Header() {
+  return (
+    <div className="headerContainer">
+      <h1 className="pageTitle">Queen of Cards</h1>
+      <a href="#" className="viewAllQueensButton">
+        View All Queens
+      </a>
+    </div>
+  );
+}
