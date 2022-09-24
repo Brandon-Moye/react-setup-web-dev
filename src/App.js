32e14daaf5c0126dfca3e44f5b1615d7a593559e
Brandon Moye <brandon.m.moye@gmail.com>
2022-09-24 14:42:59 -0500

    commint on "master"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Instructions from "./components/Instructions";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <Header />
       <Instructions />
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,5 @@
+import "./searchStyle.css";
+
+export default function Search() {
+  return <input className="searchBar" placeholder="Search Queen"></input>;
+}
