d2913e2dfaf2c8d59425b156bd4f9672060bbc90
Brandon Moye <brandon.m.moye@gmail.com>
2022-09-27 19:18:22 -0500

    can add queens to array to active-API-branch

--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -1,13 +1,16 @@
 import "./cardDisplaysStyle.css";
 import React from "react";
+import { nanoid } from "nanoid";
 
 export default function CardDisplays() {
-  const [myFavQueens, setMyFavQueens] = React.useState({
-    image: "",
-    name: "",
-    winner: "",
-    congenial: "",
-  });
+  const [myFavQueens, setMyFavQueens] = React.useState([
+    {
+      image: "",
+      queenName: "",
+      winner: "",
+      congenial: "",
+    },
+  ]);
 
   const [allQueens, setAllQueens] = React.useState([]);
 
@@ -23,22 +26,60 @@ export default function CardDisplays() {
     setMyFavQueens((prevQueen) => ({
       ...prevQueen,
       image: newQueen.image_url,
-      name: newQueen.name,
+      queenName: newQueen.name,
       winner: newQueen.winner ? "Yes" : "No",
       congenial: newQueen.missCongeniality ? "Yes" : "No",
     }));
   }
-  // const number = 0;
-  // const newQueen = allQueens[number].name;
-  // console.log(newQueen);
+
+  ///////
+
+  function kewie() {
+    let randomNumber = Math.floor(Math.random() * allQueens.length);
+    let newQueen = allQueens[randomNumber];
+    newQueen = [
+      {
+        id: nanoid(),
+        image: newQueen.image_url,
+        queenName: newQueen.name,
+        winner: newQueen.winner ? "Yes" : "No",
+        congenial: newQueen.missCongeniality ? "Yes" : "No",
+      },
+    ];
+    console.log(newQueen);
+    setMyFavQueens((prevQueen) => [newQueen, ...prevQueen]);
+  }
+  console.log(myFavQueens);
+  const mappedQueens = myFavQueens.map((newMap) => <p>{newMap}</p>);
+  console.log(mappedQueens);
+  console.log(typeof mappedQueens);
+
+  function kewieMAPPING() {
+    let randomNumber = Math.floor(Math.random() * allQueens.length);
+    let newQueen = allQueens[randomNumber];
+    newQueen = [
+      {
+        id: nanoid(),
+        image: newQueen.image_url,
+        queenName: newQueen.name,
+        winner: newQueen.winner ? "Yes" : "No",
+        congenial: newQueen.missCongeniality ? "Yes" : "No",
+      },
+    ];
+    console.log(newQueen);
+    setMyFavQueens((prevQueen) => [newQueen, ...prevQueen]);
+  }
+
   return (
     <div className="cardDisplaySectionContainer">
       <h3 className="myQueensContainerTitle">My Queens</h3>
       <button onClick={generateARandonQueen}>generateARandonQueen</button>
+      <button onClick={kewie}>kewie</button>
+      <button onClick={kewieMAPPING}>kewieMAPPING</button>
       <div className="myQueensContainer">
         <div className="myQueen--1">
           <img src={myFavQueens.image} className="myQueen--1Image"></img>
-          <p className="myQueen--1Name">Name: {myFavQueens.name}</p>
+          <p className="myQueen--1Name">Name: {myFavQueens.queenName}</p>
           <p className="myQueen--1IsWinner">Winner: {myFavQueens.winner}</p>
           <p className="myQueen--1IsCongenial">
             Congenial: {myFavQueens.congenial}
