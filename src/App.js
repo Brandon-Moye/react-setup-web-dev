c3cc55e7ac0556a1818fb50ccc5ee89a324d3d33
Brandon Moye <brandon.m.moye@gmail.com>
2022-09-29 18:35:02 -0500

    pulled-rendered-all-queens

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import Header from "./components/Header";
 import Instructions from "./components/Instructions";
 import Search from "./components/Search";
 import CardDisplays from "./components/CardDisplay";
+import React from "react";
 
 function App() {
+  const [allQueens, setAllQueens] = React.useState([]);
+
+  React.useEffect(function () {
+    fetch(`http://www.nokeynoshade.party/api/queens/all`)
+      .then((res) => res.json())
+      .then((data) => setAllQueens(data));
+  }, []);
+
+  const queenElements = allQueens.map((item) => {
+    return <CardDisplays item={item} />;
+  });
   return (
     <div>
       <Header />
       <Instructions />
       <Search />
-      <CardDisplays />
+      {queenElements}
     </div>
   );
 }
--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -1,61 +1,22 @@
 import "./cardDisplaysStyle.css";
 import React from "react";
 
-export default function CardDisplays() {
-  const [myFavQueens, setMyFavQueens] = React.useState([
-    // id: "",
-    // image: "",
-    // queenName: "",
-    // winner: "",
-    // congenial: "",
-  ]);
-
-  const [allQueens, setAllQueens] = React.useState([]);
-
-  React.useEffect(function () {
-    fetch(`http://www.nokeynoshade.party/api/queens/all`)
-      .then((res) => res.json())
-      .then((data) => setAllQueens(data));
-  }, []);
-
-  function generateARandonQueen() {
-    const randomNumber = Math.floor(Math.random() * allQueens.length);
-    const newQueen = allQueens[randomNumber];
-    // console.log(newQueen);
-    // setMyFavQueens((prevQueen) => [newQueen, ...prevQueen]);
-    // console.log(myFavQueens);
-    setMyFavQueens((prevQueen) => [
-      ...prevQueen,
-      newQueen,
-      // id: newQueen.id,
-      // image: newQueen.image_url,
-      // queenName: newQueen.name,
-      // winner: newQueen.winner ? "Yes" : "No",
-      // congenial: newQueen.missCongeniality ? "Yes" : "No",
-    ]);
-    console.log(myFavQueens);
-  }
-
-  // const number = 0;
-  // const newQueen = allQueens[number].name;
-  // console.log(newQueen);
+export default function CardDisplays(props) {
   return (
     <div className="cardDisplaySectionContainer">
       <h3 className="myQueensContainerTitle">My Queens</h3>
-      <button onClick={generateARandonQueen}>generateARandonQueen</button>
+      <button>generateARandonQueen</button>
       <div className="myQueensContainer">
         <div className="myQueen--1">
-          <img src={myFavQueens.image} className="myQueen--1Image"></img>
-          <p className="myQueen--1Name">Name: {myFavQueens.name}</p>
-          <p className="myQueen--1IsWinner">Winner: {myFavQueens.winner}</p>
+          <img src={props.item.image} className="myQueen--1Image"></img>
+          <p className="myQueen--1Name">Name: {props.item.name}</p>
+          <p className="myQueen--1IsWinner">
+            Winner: {props.item.winner ? "Yes" : "No"}
+          </p>
           <p className="myQueen--1IsCongenial">
-            Congenial: {myFavQueens.congenial}
+            Congenial: {props.item.missCongeniality ? "Yes" : "No"}
           </p>
         </div>
-        <div className="myQueen--2"></div>
-        <div className="myQueen--3"></div>
-        <div className="myQueen--4"></div>
-        <div className="myQueen--5"></div>
       </div>
     </div>
   );
