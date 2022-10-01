b4d4df4c7fda4a7b6773ab52f4a4815dd7be32b0
Brandon Moye <brandon.m.moye@gmail.com>
2022-10-01 08:22:59 -0500

    small edits

--- src/components/ViewAllQueens.js
+++ src/components/ViewAllQueens.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function ViewAllQueens(props) {
   function handleClick() {
-    console.log(props.item.id);
+    console.log(props.item);
   }
 
   return (
