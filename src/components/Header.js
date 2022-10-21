e13c2675f03029785c2c9c14a98a5fc1b4912f4e
Brandon Moye <brandon.m.moye@gmail.com>
2022-10-20 20:03:47 -0500

    messing-around-with-styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import "./headerStyle.css";
 export default function Header() {
   return (
-    
     <div className="headerContainer">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&display=swap"
+        rel="stylesheet"
+      />
+
       <h1 className="pageTitle">QUEEN OF CARDS</h1>
       <a href="#" className="viewAllQueensButton">
         View All Queens
