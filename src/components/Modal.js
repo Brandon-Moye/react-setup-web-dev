12bbc09f46b33ad6c80aab97960a8641470024c5
Brandon Moye <brandon.m.moye@gmail.com>
2022-10-31 19:10:36 -0500

    activate-modal-without-btn-needed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,15 +8,30 @@ const modalStyles = {
   transform: "translate(-50%, -50%)",
   background: "#FFF",
   padding: "50px",
-  zIndex: 1000,
+};
+
+const overlayStyles = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  background: "rgba(0,0,0,0.7)",
 };
 
 export default function Modal(props) {
   if (!props.open) return null;
-  return (
-    <div style={modalStyles} className="modalContainer">
-      <div className="modal">Too Many Queens</div>
-      <button onClick={props.onClose}>Close Modal</button>
-    </div>
+  return ReactDOM.createPortal(
+    <div className="modalContainer">
+      <div style={overlayStyles} />
+      <div style={modalStyles}>
+        <div className="modal">She Done Already Done Had Herses</div>
+        <span>
+          If you'd like to add another queen, sashay at least one away first
+        </span>
+        <button onClick={props.onClose}>Close</button>
+      </div>
+    </div>,
+    document.getElementById("portal")
   );
 }
